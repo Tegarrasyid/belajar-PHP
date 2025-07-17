2b33e2054ef137c8739b2fa5b6ef86ee99c7f92b
Tegar <tegarrasyid6@gmail.com>
2025-07-17 10:12:56 +0700

    memperbarui isi

--- belajar.php
+++ belajar.php
@@ -5,7 +5,10 @@
     <title>Belajar PHP</title>
     <style>
         body {
-            background: linear-gradient(to right, #4facfe, #00f2fe);
+            background-image: url('https://img.pikbest.com/backgrounds/20241226/a-captivating-abstract-design-with-flowing-translucent-waves-of-vibrant-colors-ranging_11303080.jpg!w700wp'); /* Ganti dengan nama file gambarmu */
+            background-size: cover;       
+            background-position: center;  
+            background-repeat: no-repeat; 
             font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
             display: flex;
             justify-content: center;
@@ -14,21 +17,20 @@
             margin: 0;
         }
         .kotak {
-            background-color: white;
+            background-color: #7d94a3ff;
             padding: 30px 40px;
             border-radius: 15px;
-            box-shadow: 0 8px 16px rgba(0,0,0,0.2);
-            text-align: center;
+            box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
         }
         .kotak h1 {
-            color: #333;
+            color: #f1f1f1;
             font-size: 24px;
         }
     </style>
 </head>
 <body>
     <div class="kotak">
-        <h1><?php echo "saya sedang belajar bahasa PHP"; ?></h1>
+        <h1><?php echo "Alhamdulillah saya dapat belajar bahasa PHP"; ?></h1>
     </div>
 </body>
 </html>
--- latihan01.php
+++ latihan01.php
@@ -1,36 +1,37 @@
-<!DOCTYPE html>
-<html lang="id">
-<head>
-    <meta charset="UTF-8">
-    <title>Belajar PHP</title>
-    <style>
-        body {
-            background-image: url('https://img.pikbest.com/backgrounds/20241226/a-captivating-abstract-design-with-flowing-translucent-waves-of-vibrant-colors-ranging_11303080.jpg!w700wp'); /* Ganti dengan nama file gambarmu */
-            background-size: cover;       
-            background-position: center;  
-            background-repeat: no-repeat; 
-            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-        }
-        .kotak {
-            background-color: #7d94a3ff;
-            padding: 30px 40px;
-            border-radius: 15px;
-            box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
-        }
-        .kotak h1 {
-            color: #f1f1f1;
-            font-size: 24px;
-        }
-    </style>
-</head>
-<body>
-    <div class="kotak">
-        <h1><?php echo "Alhamdulillah saya dapat belajar bahasa PHP"; ?></h1>
-    </div>
-</body>
-</html>
+<?php
+$a = 1;
+$b = 2;
+$c = 3;
+define(constant_name : "Oke", value : "Selamat dating");
+
+echo "Oke";
+echo "<br>";
+echo "1. Belajar Variabel Bahasa PHP <br>";
+
+if ($a > 2 && $b == 7) {
+    echo "Oke";
+} else {
+    echo $c;
+}
+echo "<br>";
+
+for ($d=0; $d<2; $d++) { 
+
+    echo $d;
+}
+echo "<br>";
+echo "<br>";
+
+$d = 0;
+while ($d < 2) {
+    echo "hasil dari perintah while ";
+    echo "<br>";
+    echo $d;
+    echo "<br>";
+    $d++;
+}
+// echo "O o $d lebih dari sama dengan 2";
+
+
+
+?>
