a2f3676e9ea3821d8634550d5084bdb03460a79c
Tegar <tegarrasyid6@gmail.com>
2025-07-17 09:08:49 +0700

    code awal

---
+++ belajar.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Belajar PHP</title>
+    <style>
+        body {
+            background: linear-gradient(to right, #4facfe, #00f2fe);
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        .kotak {
+            background-color: white;
+            padding: 30px 40px;
+            border-radius: 15px;
+            box-shadow: 0 8px 16px rgba(0,0,0,0.2);
+            text-align: center;
+        }
+        .kotak h1 {
+            color: #333;
+            font-size: 24px;
+        }
+    </style>
+</head>
+<body>
+    <div class="kotak">
+        <h1><?php echo "saya sedang belajar bahasa PHP"; ?></h1>
+    </div>
+</body>
+</html>
---
+++ latihan01.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Belajar PHP</title>
+    <style>
+        body {
+            background-image: url('https://img.pikbest.com/backgrounds/20241226/a-captivating-abstract-design-with-flowing-translucent-waves-of-vibrant-colors-ranging_11303080.jpg!w700wp'); /* Ganti dengan nama file gambarmu */
+            background-size: cover;       
+            background-position: center;  
+            background-repeat: no-repeat; 
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        .kotak {
+            background-color: #7d94a3ff;
+            padding: 30px 40px;
+            border-radius: 15px;
+            box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+        }
+        .kotak h1 {
+            color: #f1f1f1;
+            font-size: 24px;
+        }
+    </style>
+</head>
+<body>
+    <div class="kotak">
+        <h1><?php echo "Alhamdulillah saya dapat belajar bahasa PHP"; ?></h1>
+    </div>
+</body>
+</html>
